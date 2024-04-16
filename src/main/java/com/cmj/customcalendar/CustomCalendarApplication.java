ffe55968159ac6a474cb2fc0f37181a8c14c1cfe
ChanMiJung <prinpice@naver.com>
2024-04-16 23:33:36 +0900

    FEAT: 신규 프로젝트 생성 및 디렉토리 생성

---
+++ src/main/java/com/cmj/customcalendar/CustomCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.cmj.customcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmj/customcalendar/CustomCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmj.customcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
